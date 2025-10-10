24d8cd9882740bdf566bdae243d460f1d0122414
priyanshuprafful <priyanshuprafful@gmail.com>
2025-10-10 21:57:55 +0530

    Normal Code Change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "It is a easy project and I am making it complex, silly Priyanshu! DevOps Cloud Lab Running."
+    return "Learning Github Actions , AWS,DOCKER,EKS , making a simple project from scratch using perplexity ,."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
