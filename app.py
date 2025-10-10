c4675cc4afb1ae9e8a2a18cf6fef3dd014b36120
priyanshuprafful <priyanshuprafful@gmail.com>
2025-10-10 19:26:45 +0530

    Initial commit: minimal Flask app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "It is a easy project and I am making it complex  , silly Priyanshu! DevOps Cloud Lab Running."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
